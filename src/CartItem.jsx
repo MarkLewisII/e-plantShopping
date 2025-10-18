ce83858da06aebfbba91881dcd11882abcfb3ba7
MarkLewisII <mark.lewis070301@gmail.com>
2025-10-18 17:53:36 -0400

    halfway through task3

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -8,21 +8,35 @@ const CartItem = ({ onContinueShopping }) => {
   const dispatch = useDispatch();
 
   // Calculate total amount for all products in the cart
-  const calculateTotalAmount = () => {
- 
+  const calculateTotalAmount = (cart) => {
+    let total = 0;
+    cart.forEach(() => {
+        let quantity = cart.quantity;
+        let cost = parseFloat(item.cost.substring(1)) * quantity;
+        total += cost;
+    })
+
+    return total;
   };
 
   const handleContinueShopping = (e) => {
-   
+    onContinueShopping(e);
   };
 
-
+ const handleCheckoutShopping = (e) => {
+    alert('Functionality to be added for future reference');
+ };
 
   const handleIncrement = (item) => {
+    dispatch(updateQuantity({ name: item.name, quantity: item.quantity + 1 }));
   };
 
   const handleDecrement = (item) => {
-   
+    if (item.quantity > 1) {
+        dispatch(updateQuantity({name: item.name, quantity: item.quantity - 1}));
+    } else {
+        dispatch(removeItem());
+    }
   };
 
   const handleRemove = (item) => {
